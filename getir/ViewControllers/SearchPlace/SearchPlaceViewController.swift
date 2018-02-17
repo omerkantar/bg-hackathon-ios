075a4b53d072499f4eb89633cba98729458d2309
omer <iomerkantar@gmail.com>
2018-02-17 10:55:10 +0300

    develop: Search Place view controller design edildi.

--- getir/ViewControllers/SearchPlace/SearchPlaceViewController.swift
+++ getir/ViewControllers/SearchPlace/SearchPlaceViewController.swift
@@ -12,13 +12,56 @@ class SearchPlaceViewController: BaseViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
+    var searchBar: UISearchBar?
+    var searchBarPlaceholder: String?
+    
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        desigingSearchNavigationBar()
     }
     
+    // MARK: - NavigationBar Search
+    func desigingSearchNavigationBar() {
+        DispatchQueue.main.async {
+            let screenWidth: CGFloat = UIScreen.main.bounds.width
+            self.searchBar = UISearchBar(frame: CGRect(x: 20.0, y: 20.0, width: screenWidth, height: 44.0))
+            self.searchBar?.searchBarStyle = UISearchBarStyle.minimal
+            self.searchBar?.delegate = self
+            self.searchBar?.placeholder = self.searchBarPlaceholder
+            self.navigationItem.titleView = self.searchBar
+        }
+    }
 
 }
 
+
+// MARK: - UISearchBarDelegate
+extension SearchPlaceViewController: UISearchBarDelegate {
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        guard searchText.count > 1 else {
+//            self.filteredList = self.placeList
+//            reloadFilteredData()
+            return
+        }
+        
+//        let list = self.placeList?.lazy.filter({ (place) -> Bool in
+//            if let name = place.name {
+//                return name.lowercased().contains(searchText.lowercased())
+//            }
+//            return false
+//        })
+//
+//        if let list = list {
+//            self.filteredList = Array(list)
+//            reloadFilteredData()
+//        } else {
+//            self.filteredList = [Place]()
+//            reloadFilteredData()
+//        }
+    }
+}
 
